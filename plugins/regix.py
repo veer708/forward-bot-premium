53e6a6bc1a074518e67f28ea537d69a5a5dc6b2a
veer708 <veerahirwal51@gmail.com>
2026-02-07 15:20:15 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -207,7 +207,7 @@ async def edit(msg, title, status, sts):
 
    text = TEXT.format(i.fetched, i.total_files, i.duplicate, i.deleted, i.skip, status, percentage, estimated_total_time, progress)
    if status in ["cancelled", "completed"]:
-      button.append([InlineKeyboardButton('📢 Updates', url='https://t.me/Stubborn1223')])
+      button.append([InlineKeyboardButton('📢 Updates', url='https://t.me/+00WJNYJVl2ExNjZl')])
 
    else:
       button.append([InlineKeyboardButton('✖️ Cancel ✖️', 'terminate_frwd')])
